fbf28a787f160211a48ca393c9c832185e4c12d9
Lucas Santos <50067749836@escola928.edu>
2025-08-13 08:34:49 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Social-senai',
+  webDir: 'www'
+};
+
+export default config;
